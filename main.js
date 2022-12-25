d365b404be87d5ec112adaac5c59f175d2d91322
NguyenTanPil <traila1008@gmail.com>
2022-12-25 21:53:30 +0700

    Revert "Add isBoolean function"
    This reverts commit fb47351fed1d0258423cd581f4f2f6a609631c31.

--- main.js
+++ main.js
@@ -3,5 +3,3 @@ const isOddNumber = (number) => number % 2 === 1;
 const isEvenNumber = (number) => number % 2 === 0;
 
 const isArray = (value) => Array.isArray(value);
-
-const isBoolean = (value) => !!value;
