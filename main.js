fb47351fed1d0258423cd581f4f2f6a609631c31
NguyenTanPil <traila1008@gmail.com>
2022-12-25 21:53:08 +0700

    Add isBoolean function

--- main.js
+++ main.js
@@ -3,3 +3,5 @@ const isOddNumber = (number) => number % 2 === 1;
 const isEvenNumber = (number) => number % 2 === 0;
 
 const isArray = (value) => Array.isArray(value);
+
+const isBoolean = (value) => !!value;
